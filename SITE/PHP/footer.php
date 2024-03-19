8b715ae6dda905ed312456e92e08307bcd7308a8
Florentms <florent.maillardsalin@viacesi.fr>
2024-03-19 11:39:46 +0100

    Ajout d'un footer et d'un header centralisés par php

---
+++ SITE/PHP/footer.php
@@ -0,0 +1,18 @@
+<?php 
+    echo'<footer class="footer">
+            <div class="footer-group">
+                <div class="logo"><img src="Assets/logo avec texte.png" alt="logo-merge"></div>
+            </div>
+            <div class="footer-links">
+                <ul class="footer-list">
+                    <li><a href="#" class="footer-link">Conditions générales d'."'".'utilisation</a></li>
+                    <li><a href="#" class="footer-link">Politique de confidentialité</a></li>
+                    <li><a href="#" class="footer-link">Contact</a></li>
+                    <li><a href="#" class="footer-link">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="footer-copyright">
+                <p class="footer-text">&#169; 2024 Nom - Tous Droits Réservés.</p>
+            </div>
+        </footer>';
+?>
---
+++ SITE/PHP/header.php
@@ -0,0 +1,15 @@
+<?php
+    echo'<header>
+            <nav class="navbar">
+                <img id="nav-logo" src="Assets/logo avec texte.png">
+
+                <ul class="nav-links">
+                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="#.html">Trouver un stage</a></li>
+                    <li><a href="#.html">Trouver un pilote</a></li>
+                    <li><a href="#.html">Trouver une entreprise</a></li>
+                </ul>
+                <img src="Assets/user-circle.svg" alt="User icon" class="user-icon">
+            </nav>
+        </header>';
+?>
