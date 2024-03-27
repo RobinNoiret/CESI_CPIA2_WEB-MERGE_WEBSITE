2d4f7edb4edbf931d6691923bfd0f032484ed999
Florentms <florent.maillardsalin@viacesi.fr>
2024-03-27 14:00:43 +0100

    Création de la structure de base de mainView

---
+++ MERGE_WEB_APP/Views/mainView.php
@@ -0,0 +1,9 @@
+// Set variables to display with smarty 
+<?php 
+    require_once 'libs/Smarty.class.php';
+
+    $smarty = new Smarty();
+
+    if (true){
+        $smarty->display('/*path to tpl page to display*/');
+    }
