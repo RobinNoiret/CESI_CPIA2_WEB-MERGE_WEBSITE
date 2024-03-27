50a403201c861171bf1d76dc1ced3bcd11348e51
FlorentMS <150123800+FlorentMS@users.noreply.github.com>
2024-03-27 15:00:59 +0100

    ajout du fichier de création des tables de la base sans peuplement

---
+++ BDD/mergedatabase_CreateTable.sql
@@ -0,0 +1,368 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2024 at 09:38 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mergedatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `addressID` int(11) NOT NULL,
+  `streetName` varchar(40) NOT NULL,
+  `streetNum` smallint(6) NOT NULL,
+  `companyID` int(11) NOT NULL,
+  `cityID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidatures`
+--
+
+CREATE TABLE `candidatures` (
+  `offerID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `coverLetter` text DEFAULT NULL,
+  `CV` blob DEFAULT NULL,
+  `accepted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `cityID` int(11) NOT NULL,
+  `cityName` varchar(50) NOT NULL,
+  `postalCode` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `classID` int(11) NOT NULL,
+  `className` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE `companies` (
+  `companyID` int(11) NOT NULL,
+  `companyName` varchar(25) DEFAULT NULL,
+  `activityArea` varchar(20) DEFAULT NULL,
+  `visibility` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluations`
+--
+
+CREATE TABLE `evaluations` (
+  `companyID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `grade` decimal(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offers`
+--
+
+CREATE TABLE `offers` (
+  `offerID` int(11) NOT NULL,
+  `internDuration` smallint(6) DEFAULT NULL,
+  `publicationDate` date DEFAULT NULL,
+  `placesNumber` smallint(6) DEFAULT NULL,
+  `remunerationBasis` decimal(10,2) DEFAULT NULL,
+  `addressID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skilloffer`
+--
+
+CREATE TABLE `skilloffer` (
+  `offerID` int(11) NOT NULL,
+  `skillID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `skillID` int(11) NOT NULL,
+  `skill` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userclass`
+--
+
+CREATE TABLE `userclass` (
+  `userID` int(11) NOT NULL,
+  `classID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `userLastName` varchar(25) DEFAULT NULL,
+  `userFirstName` varchar(25) DEFAULT NULL,
+  `userStatus` enum('Piolte','Etudiant','Admin') DEFAULT NULL,
+  `userLogin` varchar(20) DEFAULT NULL,
+  `userPassword` varchar(100) DEFAULT NULL,
+  `cityID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `offerID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`addressID`),
+  ADD KEY `companyID` (`companyID`),
+  ADD KEY `cityID` (`cityID`);
+
+--
+-- Indexes for table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD PRIMARY KEY (`offerID`,`userID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`cityID`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`classID`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`companyID`);
+
+--
+-- Indexes for table `evaluations`
+--
+ALTER TABLE `evaluations`
+  ADD PRIMARY KEY (`companyID`,`userID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `offers`
+--
+ALTER TABLE `offers`
+  ADD PRIMARY KEY (`offerID`),
+  ADD KEY `addressID` (`addressID`);
+
+--
+-- Indexes for table `skilloffer`
+--
+ALTER TABLE `skilloffer`
+  ADD PRIMARY KEY (`offerID`,`skillID`),
+  ADD KEY `skillID` (`skillID`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`skillID`);
+
+--
+-- Indexes for table `userclass`
+--
+ALTER TABLE `userclass`
+  ADD PRIMARY KEY (`userID`,`classID`),
+  ADD KEY `classID` (`classID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `userLogin` (`userLogin`),
+  ADD KEY `cityID` (`cityID`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`offerID`,`userID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `addressID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `cityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `classID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `companyID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `offers`
+--
+ALTER TABLE `offers`
+  MODIFY `offerID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `skillID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`companyID`) REFERENCES `companies` (`companyID`),
+  ADD CONSTRAINT `addresses_ibfk_2` FOREIGN KEY (`cityID`) REFERENCES `cities` (`cityID`);
+
+--
+-- Constraints for table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD CONSTRAINT `candidatures_ibfk_1` FOREIGN KEY (`offerID`) REFERENCES `offers` (`offerID`),
+  ADD CONSTRAINT `candidatures_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `evaluations`
+--
+ALTER TABLE `evaluations`
+  ADD CONSTRAINT `evaluations_ibfk_1` FOREIGN KEY (`companyID`) REFERENCES `companies` (`companyID`),
+  ADD CONSTRAINT `evaluations_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `offers`
+--
+ALTER TABLE `offers`
+  ADD CONSTRAINT `offers_ibfk_1` FOREIGN KEY (`addressID`) REFERENCES `addresses` (`addressID`);
+
+--
+-- Constraints for table `skilloffer`
+--
+ALTER TABLE `skilloffer`
+  ADD CONSTRAINT `skilloffer_ibfk_1` FOREIGN KEY (`offerID`) REFERENCES `offers` (`offerID`),
+  ADD CONSTRAINT `skilloffer_ibfk_2` FOREIGN KEY (`skillID`) REFERENCES `skills` (`skillID`);
+
+--
+-- Constraints for table `userclass`
+--
+ALTER TABLE `userclass`
+  ADD CONSTRAINT `userclass_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  ADD CONSTRAINT `userclass_ibfk_2` FOREIGN KEY (`classID`) REFERENCES `classes` (`classID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`cityID`) REFERENCES `cities` (`cityID`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`offerID`) REFERENCES `offers` (`offerID`),
+  ADD CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
