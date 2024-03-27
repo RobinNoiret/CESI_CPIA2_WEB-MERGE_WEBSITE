cf83ed4185502678ab66c4e46934f5bf3d8f4a03
Florentms <florent.maillardsalin@viacesi.fr>
2024-03-27 20:22:40 +0100

    Creation du modèle homeDataModel.php avec l'impémentation de ces méthodes

---
+++ MERGE_WEB_APP/Models/homeDataModel.php
@@ -0,0 +1,38 @@
+<?php
+    include 'connectEntity.php';
+
+    class piloteModel extends connectToDB{
+
+        public function countPilotes(){
+        $request = $this->db->prepare("SELECT count(userID) AS NumberOfPilotes FROM users where userStatus='Pilote';");
+
+        $this->tryToExecute($request);                  
+        $datas = $request->fetch();  
+
+        // Call functions to set datas if needed
+
+        return $datas;
+        }
+
+        public function countStudents(){
+            $request = $this->db->prepare("SELECT count(userID) AS NumberOfStudents FROM users where userStatus='Etudiant';");
+    
+            $this->tryToExecute($request);                  
+            $datas = $request->fetch();     
+    
+            // Call functions to set datas if needed
+    
+            return $datas;
+            }
+
+        public function countCompanies(){
+            $request = $this->db->prepare("SELECT count(companyName) AS NumberOfCompanies FROM companies where visibility=1;");
+    
+            $this->tryToExecute($request);                  
+            $datas = $request->fetch();  
+    
+            // Call functions to set datas if needed
+    
+            return $datas;
+            }
+    }
