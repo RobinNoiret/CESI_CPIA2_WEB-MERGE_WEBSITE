4eb3fccbefa4a004fc45d84ab7ae68585b916849
Florentms <florent.maillardsalin@viacesi.fr>
2024-04-02 22:34:56 +0200

    Ajout de flex paramètre pour le body le header le footer et le content

--- MERGE_WEB_APP/templates_c/3b238e60590e889026f1dcde3ca3fa2b6e10850a_0.file.templateBase.tpl.php
+++ MERGE_WEB_APP/templates_c/3b238e60590e889026f1dcde3ca3fa2b6e10850a_0.file.templateBase.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2024-04-02 18:32:57
+/* Smarty version 3.1.36, created on 2024-04-02 22:26:32
   from 'C:\Users\flore\OneDrive\Documents\Cours\Cesi\A2\04 - Dev Web\Projet\Projet-git-part\CESI_CPIA2_WEB_PROJECT\MERGE_WEB_APP\Views\tpl\templateBase.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_660c3339771389_34683111',
+  'unifunc' => 'content_660c69f8be5726_00927504',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '3b238e60590e889026f1dcde3ca3fa2b6e10850a' => 
     array (
       0 => 'C:\\Users\\flore\\OneDrive\\Documents\\Cours\\Cesi\\A2\\04 - Dev Web\\Projet\\Projet-git-part\\CESI_CPIA2_WEB_PROJECT\\MERGE_WEB_APP\\Views\\tpl\\templateBase.tpl',
-      1 => 1712075505,
+      1 => 1712089554,
       2 => 'file',
     ),
   ),
@@ -24,7 +24,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../tpl/footer.tpl' => 1,
   ),
 ),false)) {
-function content_660c3339771389_34683111 (Smarty_Internal_Template $_smarty_tpl) {
+function content_660c69f8be5726_00927504 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 ?>
@@ -34,25 +34,25 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 
     <head> 
     <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_162862849660c333975e368_62544071', 'head');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1846709308660c69f8a59420_07746487', 'head');
 ?>
 
     </head> 
 
    	<body>
     <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_677604659660c3339763983_55858643', 'body');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_266433539660c69f8a5f5c9_71932142', 'body');
 ?>
 
     </body> 
 </html> <?php }
 /* {block 'head'} */
-class Block_162862849660c333975e368_62544071 extends Smarty_Internal_Block
+class Block_1846709308660c69f8a59420_07746487 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'head' => 
   array (
-    0 => 'Block_162862849660c333975e368_62544071',
+    0 => 'Block_1846709308660c69f8a59420_07746487',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -71,12 +71,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block 'head'} */
 /* {block 'body'} */
-class Block_677604659660c3339763983_55858643 extends Smarty_Internal_Block
+class Block_266433539660c69f8a5f5c9_71932142 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'body' => 
   array (
-    0 => 'Block_677604659660c3339763983_55858643',
+    0 => 'Block_266433539660c69f8a5f5c9_71932142',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -85,14 +85,15 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 
         <?php $_smarty_tpl->_subTemplateRender("file:../tpl/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-
-        <?php if ($_smarty_tpl->tpl_vars['page']->value == 'home') {?>
-            <?php $_smarty_tpl->_subTemplateRender("file:../tpl/homeContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+        <div class="Content">
+            <?php if ($_smarty_tpl->tpl_vars['page']->value == 'home') {?>
+                <?php $_smarty_tpl->_subTemplateRender("file:../tpl/homeContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-        <?php } elseif ($_smarty_tpl->tpl_vars['page']->value == 'connexion') {?>
-            <?php $_smarty_tpl->_subTemplateRender("file:../tpl/connexionContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+            <?php } elseif ($_smarty_tpl->tpl_vars['page']->value == 'connexion') {?>
+                <?php $_smarty_tpl->_subTemplateRender("file:../tpl/connexionContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-        <?php }?>
+            <?php }?>
+        </div>
 
         <?php $_smarty_tpl->_subTemplateRender("file:../tpl/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
