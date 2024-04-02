2978fc42e81f5a1e1f5dfa0320f9695107584f0d
Florentms <florent.maillardsalin@viacesi.fr>
2024-04-02 18:34:20 +0200

    Correct the header appearence header.css

---
+++ MERGE_WEB_APP/templates_c/3b238e60590e889026f1dcde3ca3fa2b6e10850a_0.file.templateBase.tpl.php
@@ -0,0 +1,104 @@
+<?php
+/* Smarty version 3.1.36, created on 2024-04-02 18:32:57
+  from 'C:\Users\flore\OneDrive\Documents\Cours\Cesi\A2\04 - Dev Web\Projet\Projet-git-part\CESI_CPIA2_WEB_PROJECT\MERGE_WEB_APP\Views\tpl\templateBase.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.36',
+  'unifunc' => 'content_660c3339771389_34683111',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '3b238e60590e889026f1dcde3ca3fa2b6e10850a' => 
+    array (
+      0 => 'C:\\Users\\flore\\OneDrive\\Documents\\Cours\\Cesi\\A2\\04 - Dev Web\\Projet\\Projet-git-part\\CESI_CPIA2_WEB_PROJECT\\MERGE_WEB_APP\\Views\\tpl\\templateBase.tpl',
+      1 => 1712075505,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:../tpl/header.tpl' => 1,
+    'file:../tpl/homeContent.tpl' => 1,
+    'file:../tpl/connexionContent.tpl' => 1,
+    'file:../tpl/footer.tpl' => 1,
+  ),
+),false)) {
+function content_660c3339771389_34683111 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+
+<!doctype html> 
+<html lang="fr"> 
+
+    <head> 
+    <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_162862849660c333975e368_62544071', 'head');
+?>
+
+    </head> 
+
+   	<body>
+    <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_677604659660c3339763983_55858643', 'body');
+?>
+
+    </body> 
+</html> <?php }
+/* {block 'head'} */
+class Block_162862849660c333975e368_62544071 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'head' => 
+  array (
+    0 => 'Block_162862849660c333975e368_62544071',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+		<meta charset="utf-8"> 	
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="Views/CSS/main.css" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz@0,9..40;1,9..40&display=swap" rel="stylesheet">
+        
+        <title> <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ </title>	
+    <?php
+}
+}
+/* {/block 'head'} */
+/* {block 'body'} */
+class Block_677604659660c3339763983_55858643 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'body' => 
+  array (
+    0 => 'Block_677604659660c3339763983_55858643',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+        <?php $_smarty_tpl->_subTemplateRender("file:../tpl/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+        <?php if ($_smarty_tpl->tpl_vars['page']->value == 'home') {?>
+            <?php $_smarty_tpl->_subTemplateRender("file:../tpl/homeContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        <?php } elseif ($_smarty_tpl->tpl_vars['page']->value == 'connexion') {?>
+            <?php $_smarty_tpl->_subTemplateRender("file:../tpl/connexionContent.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        <?php }?>
+
+        <?php $_smarty_tpl->_subTemplateRender("file:../tpl/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+        <?php
+}
+}
+/* {/block 'body'} */
+}
---
+++ MERGE_WEB_APP/templates_c/cdb50ddc8672c40abf88474b37bb8950562c621a_0.file.homeContent.tpl.php
@@ -0,0 +1,143 @@
+<?php
+/* Smarty version 3.1.36, created on 2024-04-02 18:32:57
+  from 'C:\Users\flore\OneDrive\Documents\Cours\Cesi\A2\04 - Dev Web\Projet\Projet-git-part\CESI_CPIA2_WEB_PROJECT\MERGE_WEB_APP\Views\tpl\homeContent.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.36',
+  'unifunc' => 'content_660c3339a36444_90868632',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'cdb50ddc8672c40abf88474b37bb8950562c621a' => 
+    array (
+      0 => 'C:\\Users\\flore\\OneDrive\\Documents\\Cours\\Cesi\\A2\\04 - Dev Web\\Projet\\Projet-git-part\\CESI_CPIA2_WEB_PROJECT\\MERGE_WEB_APP\\Views\\tpl\\homeContent.tpl',
+      1 => 1712056066,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="landing">
+
+    <!-- ___________________________________________ Image de fond ___________________________________________ -->
+
+    <div id="landing-pictureBG">
+        <img id="picture" src="Views/Assets/Picture backgroundhome-firstpicture-min.png">
+    </div>
+
+    <!-- ____________________________________________ Bloc stats ____________________________________________ -->
+
+    <div class="landing-stats">
+        <div class="stats-block" id="stats-students">
+            <div class="stats-number"><?php echo $_smarty_tpl->tpl_vars['studentsNumber']->value;?>
+</div>
+            <div class="stats-text">Etudiants</div>
+        </div>
+
+        <div class="stats-block" id="stats-companies">
+            <div class="stats-number"><?php echo $_smarty_tpl->tpl_vars['companiesNumber']->value;?>
+</div>
+            <div class="stats-text">Entreprises</div>
+        </div>
+
+        <div class="stats-block" id="stats-pilotes">
+            <div class="stats-number"><?php echo $_smarty_tpl->tpl_vars['pilotesNumber']->value;?>
+</div>
+            <div class="stats-text">Pilotes</div>
+        </div>
+    </div>
+
+    <!-- __________________________________________ Bloc domaines __________________________________________ -->
+
+    <div class="landing-domains">
+        <h2>Quels domaines ?</h2>
+
+        <div class="domains">
+            <div class="domain" id="domain-IT">
+                <a href="error-accèsInternet.html">
+                <img class="domain-pictures" src="Views/Assets/TechnologyIT.png">
+                </a>
+                <h4 class="domain-title">IT</h4>
+            </div>
+
+            <div class="domain" id="domain-services">
+                <a href="error-accèsServeur.html">
+                <img class="domain-pictures" src="Views/Assets/Serviceservice.png">
+                </a>
+                <h4 class="domain-title">Services</h4>
+            </div>
+
+            <div class="domain" id="domain-Health">
+                <a href="error-FlorentSauvage.html">
+                <img class="domain-pictures" src="Views/Assets/Health Care - Health Researchhealth.png">
+                </a>
+                <h4 class="domain-title">Santé</h4>
+            </div>
+
+            <div class="domain" id="domain-Finances">
+                <a href="error-pageDoesntExist.html">
+                <img class="domain-pictures" src="Views/Assets/Business Planbusiness.svg">
+                </a>
+                <h4 class="domain-title">Finances</h4>
+            </div>
+        </div>
+    </div>
+
+    <!-- ________________________________________ Bloc articles __________________________________________ -->
+
+    <div class="landing-articles">
+        <article class="articles-pict_right">
+            <div class="articles-text">
+                <h4>Explorez et trouver votre voie</h4>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus egestas ex, id tempor sem maximus id. 
+                    Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+
+                <a class="CTA_secondaire" href="#">Découvrir les fiches métiers</a>
+            </div>
+
+            <img class="article-picture" src="Views/Assets/Placeholder imageexplorez et trouver votre voie.png">
+
+        </article>
+
+        <article class="articles-pict_left">
+            <img class="article-picture" src="Views/Assets/Placeholder imagedéveloppez votre réseau.png">
+            
+            <div class="articles-text">
+                <h4>Développez votre réseau</h4>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus egestas ex, id tempor sem maximus id. 
+                    Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+
+                <a class="CTA_secondaire" href="#">Découvrir les évènements</a>
+            </div>
+            <img class="article-picture-responsive" src="Views/Assets/Placeholder imagedéveloppez votre réseau.png">
+        </article>
+
+        <article class="articles-pict_right">
+            <div class="articles-text">
+                <h4>Préparez-vous !</h4>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus egestas ex, id tempor sem maximus id. 
+                    Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+
+                <a class="CTA_secondaire" href="#">Découvrir comment vous préparez</a>
+            </div>
+
+            <img class="article-picture" src="Views/Assets/Placeholder imagepréparez vous.png">
+        </article>
+
+    </div>
+
+</div><?php }
+}
