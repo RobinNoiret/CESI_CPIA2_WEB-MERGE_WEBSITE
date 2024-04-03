6c918961760e9b815a185215c0b04e80c819505e
Florentms <florent.maillardsalin@viacesi.fr>
2024-04-03 09:18:50 +0200

    Génération de nouveau documents dans template_c

--- MERGE_WEB_APP/templates_c/3b238e60590e889026f1dcde3ca3fa2b6e10850a_0.file.templateBase.tpl.php
+++ MERGE_WEB_APP/templates_c/3b238e60590e889026f1dcde3ca3fa2b6e10850a_0.file.templateBase.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2024-04-02 22:26:32
+/* Smarty version 3.1.36, created on 2024-04-03 09:18:04
   from 'C:\Users\flore\OneDrive\Documents\Cours\Cesi\A2\04 - Dev Web\Projet\Projet-git-part\CESI_CPIA2_WEB_PROJECT\MERGE_WEB_APP\Views\tpl\templateBase.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_660c69f8be5726_00927504',
+  'unifunc' => 'content_660d02acace512_00543093',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '3b238e60590e889026f1dcde3ca3fa2b6e10850a' => 
     array (
       0 => 'C:\\Users\\flore\\OneDrive\\Documents\\Cours\\Cesi\\A2\\04 - Dev Web\\Projet\\Projet-git-part\\CESI_CPIA2_WEB_PROJECT\\MERGE_WEB_APP\\Views\\tpl\\templateBase.tpl',
-      1 => 1712089554,
+      1 => 1712128664,
       2 => 'file',
     ),
   ),
@@ -24,7 +24,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../tpl/footer.tpl' => 1,
   ),
 ),false)) {
-function content_660c69f8be5726_00927504 (Smarty_Internal_Template $_smarty_tpl) {
+function content_660d02acace512_00543093 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 ?>
@@ -34,25 +34,25 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 
     <head> 
     <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1846709308660c69f8a59420_07746487', 'head');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_315800723660d02acabbed0_30383607', 'head');
 ?>
 
     </head> 
 
    	<body>
     <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_266433539660c69f8a5f5c9_71932142', 'body');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_919749162660d02acac4892_57822684', 'body');
 ?>
 
     </body> 
 </html> <?php }
 /* {block 'head'} */
-class Block_1846709308660c69f8a59420_07746487 extends Smarty_Internal_Block
+class Block_315800723660d02acabbed0_30383607 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'head' => 
   array (
-    0 => 'Block_1846709308660c69f8a59420_07746487',
+    0 => 'Block_315800723660d02acabbed0_30383607',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -71,12 +71,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block 'head'} */
 /* {block 'body'} */
-class Block_266433539660c69f8a5f5c9_71932142 extends Smarty_Internal_Block
+class Block_919749162660d02acac4892_57822684 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'body' => 
   array (
-    0 => 'Block_266433539660c69f8a5f5c9_71932142',
+    0 => 'Block_919749162660d02acac4892_57822684',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
--- MERGE_WEB_APP/templates_c/cdb50ddc8672c40abf88474b37bb8950562c621a_0.file.homeContent.tpl.php
+++ MERGE_WEB_APP/templates_c/cdb50ddc8672c40abf88474b37bb8950562c621a_0.file.homeContent.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2024-04-02 18:32:57
+/* Smarty version 3.1.36, created on 2024-04-03 09:18:04
   from 'C:\Users\flore\OneDrive\Documents\Cours\Cesi\A2\04 - Dev Web\Projet\Projet-git-part\CESI_CPIA2_WEB_PROJECT\MERGE_WEB_APP\Views\tpl\homeContent.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_660c3339a36444_90868632',
+  'unifunc' => 'content_660d02acd98571_34313423',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'cdb50ddc8672c40abf88474b37bb8950562c621a' => 
     array (
       0 => 'C:\\Users\\flore\\OneDrive\\Documents\\Cours\\Cesi\\A2\\04 - Dev Web\\Projet\\Projet-git-part\\CESI_CPIA2_WEB_PROJECT\\MERGE_WEB_APP\\Views\\tpl\\homeContent.tpl',
-      1 => 1712056066,
+      1 => 1712128104,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl) {
+function content_660d02acd98571_34313423 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="landing">
 
     <!-- ___________________________________________ Image de fond ___________________________________________ -->
@@ -58,28 +58,32 @@ function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl)
 
         <div class="domains">
             <div class="domain" id="domain-IT">
-                <a href="error-accèsInternet.html">
+                <a href="<?php echo ($_smarty_tpl->tpl_vars['source']->value).('?page=internship&action=research&domain=IT');?>
+">
                 <img class="domain-pictures" src="Views/Assets/TechnologyIT.png">
                 </a>
                 <h4 class="domain-title">IT</h4>
             </div>
 
             <div class="domain" id="domain-services">
-                <a href="error-accèsServeur.html">
+                <a href="<?php echo ($_smarty_tpl->tpl_vars['source']->value).('?page=internshi&action=researchp&domain=Services');?>
+">
                 <img class="domain-pictures" src="Views/Assets/Serviceservice.png">
                 </a>
                 <h4 class="domain-title">Services</h4>
             </div>
 
             <div class="domain" id="domain-Health">
-                <a href="error-FlorentSauvage.html">
+                <a href="<?php echo ($_smarty_tpl->tpl_vars['source']->value).('?page=internship&action=research&domain=Santé');?>
+">
                 <img class="domain-pictures" src="Views/Assets/Health Care - Health Researchhealth.png">
                 </a>
                 <h4 class="domain-title">Santé</h4>
             </div>
 
             <div class="domain" id="domain-Finances">
-                <a href="error-pageDoesntExist.html">
+                <a href="<?php echo ($_smarty_tpl->tpl_vars['source']->value).('?page=internship&action=research&domain=Finances');?>
+">
                 <img class="domain-pictures" src="Views/Assets/Business Planbusiness.svg">
                 </a>
                 <h4 class="domain-title">Finances</h4>
@@ -99,7 +103,9 @@ function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl)
                     Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
 
-                <a class="CTA_secondaire" href="#">Découvrir les fiches métiers</a>
+                <a class="CTA_secondaire" href="https://www.emploi-store.fr/portail/parcours-conseils/choisisFuturMetier/fiche-metier">
+                    Découvrir les fiches métiers
+                </a>
             </div>
 
             <img class="article-picture" src="Views/Assets/Placeholder imageexplorez et trouver votre voie.png">
@@ -117,7 +123,9 @@ function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl)
                     Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
 
-                <a class="CTA_secondaire" href="#">Découvrir les évènements</a>
+                <a class="CTA_secondaire" href="https://www.nsalons.com/entreprises/france/">
+                    Découvrir les évènements
+                </a>
             </div>
             <img class="article-picture-responsive" src="Views/Assets/Placeholder imagedéveloppez votre réseau.png">
         </article>
@@ -131,7 +139,9 @@ function content_660c3339a36444_90868632 (Smarty_Internal_Template $_smarty_tpl)
                     Quisque dignissim neque tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
 
-                <a class="CTA_secondaire" href="#">Découvrir comment vous préparez</a>
+                <a class="CTA_secondaire" href="https://www.welcometothejungle.com/fr/articles/preparer-entretien-stage">
+                    Découvrir comment vous préparez
+                </a>
             </div>
 
             <img class="article-picture" src="Views/Assets/Placeholder imagepréparez vous.png">
