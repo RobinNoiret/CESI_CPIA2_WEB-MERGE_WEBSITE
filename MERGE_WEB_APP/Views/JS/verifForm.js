113350855b9110b7ccee0f273b745304cc25302f
Robin Noiret <119452365+RobinNoiret@users.noreply.github.com>
2024-04-05 08:28:51 +0200

    Mise à jour de la validation. Pas complète du tout

--- MERGE_WEB_APP/Views/JS/verifForm.js
+++ MERGE_WEB_APP/Views/JS/verifForm.js
@@ -1,3 +1,18 @@
+/*
+/_____________________ Expressions Régulières _____________________
+// Page de connexion
+var regexLogin = /^[a-zA-Z]+\.[a-zA-Z]+$/;
+var regexPassword = NULL;       // Pas de format sur les passwords de la BDD donc pas possible d'appliqué un REGEX
+
+// Page de  création
+var regexText = /^[a-zA-Z\s]+$/;
+var regexNumRue = /^\d+\s?[a-zA-Z]*$/;
+var regexNomRue = /^[a-zA-Z\s-]+$/;
+var regexCodePostal = /^\d{5}$/;
+
+
+*/
+
 
 //_____________________ Fonction de vérification _____________________
 function validationConnexion()
@@ -19,9 +34,23 @@ alert("Connexion réussite !");
 return true;
 }
 
+function affichageCompany()
+{
+    var CompanyIDField = document.getElementById("CompanyID");
+    console.log("J'ai bien récup les éléments");
+
+    if (CompanyIDField == "") {alert("Veuillez saisir l'identifiant");return false; }       // Test du champs identifiant
+else if (!isValidLogin(CompanyIDField)) {alert("Votre identifiant doit être un nombre entier");
+        return false;}
+}
+
 //_____________________ Fonction de validation _____________________
 function isValidLogin(LoginField) {
     var postalRegex = /^[a-zA-Z]+\.[a-zA-Z]+$/;         //Définition Regex du login
+    return postalRegex.test(LoginField.value);}               //Test du format
+
+function isValidLogin(LoginField) {
+    var postalRegex = /^\d+\s?/;                        //Définition Regex de l'ID
     return postalRegex.test(LoginField.value);}               //Test du format
 
 
