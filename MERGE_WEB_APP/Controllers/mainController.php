725d38ad0fb5025e8a1dbc4d186e8568dcb216e5
Florentms <florent.maillardsalin@viacesi.fr>
2024-04-05 09:00:15 +0200

    Ajout de blocage

--- MERGE_WEB_APP/Controllers/mainController.php
+++ MERGE_WEB_APP/Controllers/mainController.php
@@ -171,67 +171,72 @@
         }
 
         public function companiesController() {
-            $page = 'companies';
-        
-            // Inclure le modèle des entreprises
-            include_once 'Models/companyModel.php';
-        
-            // Créer une instance du modèle des entreprises
-            $companyModel = new companyModel($this->sourcePath);
-        
-            // Vérifier si un ID d'entreprise est passé en paramètre
-            if (isset($_POST['search'])) {
-                // Récupération de l'id de la company
-                $companyID = $_POST['companyID'];
-                // Vérifier si l'ID de l'entreprise est vide
-                if (!empty($companyID)) {
-                    // Appeler la méthode select() pour récupérer l'entreprise spécifique
-                    $companies = $companyModel->select($companyID); // Assurez-vous que le résultat est toujours sous forme de tableau
+            if ($this->whatIsConnect() != 'none'){              // Verify user's connexion
+                $page = 'companies';
+            
+                // Inclure le modèle des entreprises
+                include_once 'Models/companyModel.php';
+            
+                // Créer une instance du modèle des entreprises
+                $companyModel = new companyModel($this->sourcePath);
+            
+                // Vérifier si un ID d'entreprise est passé en paramètre
+                if (isset($_POST['search'])) {
+                    // Récupération de l'id de la company
+                    $companyID = $_POST['companyID'];
+                    // Vérifier si l'ID de l'entreprise est vide
+                    if (!empty($companyID)) {
+                        // Appeler la méthode select() pour récupérer l'entreprise spécifique
+                        $companies = $companyModel->select($companyID); // Assurez-vous que le résultat est toujours sous forme de tableau
+                    } else {
+                        // Appeler la méthode selectAll() pour récupérer toutes les entreprises par défaut
+                        $companies = $companyModel->selectAll();
+                    }
+                } else if (isset($_GET['action']) && $_GET['action'] == 'add') {
+                    $action = $_GET['action'];
+                    $companies = '';
+                    // Initialisation des variables d'erreur et de succès
+                    $error = '';
+                    $success = '';
+
+
+                    // Vérifier si des données de formulaire ont été soumises pour l'ajout d'une entreprise
+                    if (isset($_POST['submit'])) {
+                        // Récupérer les données du formulaire
+                        $companyName = $_POST['nomEntreprise'];
+                        $activityArea = $_POST['secteurActivite'];
+                        $streetName = $_POST['nomRue1'];
+                        $streetNum = $_POST['numRue1'];
+                        $postalCode = $_POST['codePostal1'];
+                        var_dump($_GET['submit']);
+
+                        // Appeler la méthode insert du modèle pour insérer une nouvelle entreprise
+                        $insertResult = $companyModel->insert($companyName, $activityArea, $streetName, $streetNum, $postalCode);
+
+                        // Gérer le résultat de l'insertion (message de succès ou d'erreur)
+                        // Vous pouvez rediriger ou rafraîchir la page ici après l'insertion
+                
+                        // Par exemple, afficher un message de succès ou d'erreur
+                        if ($insertResult === true) {
+                            $success = "Entreprise insérée avec succès.";
+                            echo $success;
+                        } else {
+                            $error = "Erreur lors de l'insertion de l'entreprise : " . $insertResult;
+                            echo $error;
+                        }
+
+                        // Rediriger vers la page précédente (page de l'entreprise)
+                        header("Location: http://localhost/Projet/CESI_CPIA2_WEB_PROJECT-1/MERGE_WEB_APP/?page=company");
+                        exit();
+
+                    }
                 } else {
                     // Appeler la méthode selectAll() pour récupérer toutes les entreprises par défaut
                     $companies = $companyModel->selectAll();
                 }
-            } else if (isset($_GET['action']) && $_GET['action'] == 'add') {
-                $action = $_GET['action'];
-                $companies = '';
-                // Initialisation des variables d'erreur et de succès
-                $error = '';
-                $success = '';
-
-
-                // Vérifier si des données de formulaire ont été soumises pour l'ajout d'une entreprise
-                if (isset($_POST['submit'])) {
-                    // Récupérer les données du formulaire
-                    $companyName = $_POST['nomEntreprise'];
-                    $activityArea = $_POST['secteurActivite'];
-                    $streetName = $_POST['nomRue1'];
-                    $streetNum = $_POST['numRue1'];
-                    $postalCode = $_POST['codePostal1'];
-                    var_dump($_GET['submit']);
-
-                    // Appeler la méthode insert du modèle pour insérer une nouvelle entreprise
-                    $insertResult = $companyModel->insert($companyName, $activityArea, $streetName, $streetNum, $postalCode);
-
-                    // Gérer le résultat de l'insertion (message de succès ou d'erreur)
-                    // Vous pouvez rediriger ou rafraîchir la page ici après l'insertion
-            
-                    // Par exemple, afficher un message de succès ou d'erreur
-                    if ($insertResult === true) {
-                        $success = "Entreprise insérée avec succès.";
-                        echo $success;
-                    } else {
-                        $error = "Erreur lors de l'insertion de l'entreprise : " . $insertResult;
-                        echo $error;
-                    }
-
-                    // Rediriger vers la page précédente (page de l'entreprise)
-                    header("Location: http://localhost/Projet/CESI_CPIA2_WEB_PROJECT-1/MERGE_WEB_APP/?page=company");
-                    exit();
-
-                }
-            } else {
-                // Appeler la méthode selectAll() pour récupérer toutes les entreprises par défaut
-                $companies = $companyModel->selectAll();
+            }
+            else{
+                header('Location: '.$this->sourcePath.'?page=connexion');
             }
         
             // Passer les données récupérées à la vue
